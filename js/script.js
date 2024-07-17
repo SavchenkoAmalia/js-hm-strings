b422fbb7e275750339979d65b982a2470145b96e
SavchenkoAmalia <ameli.savchenko@gmail.com>
2024-07-17 18:57:48 +0300

    add all

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const result= 5 + 5 + '5';
+console.log(result);
+
+const email = "ameli.savchenko@gmail.com";
+console.log(result.includes('@'));
+console.log(email.length);
+let word1 = 'My';
+let word2 = 'name';
+let word3 = 'is';
+// const fullName = `${word1  + word2 + word3 } Victor`;
+// console.log(fullName);
+fullName = word1 + " " + word2 + " " + word3 + ' Viktor';
+console.log(fullName);
+
+let userName = 'Максим';
+let payment = 300;
+console.log(alert(`Дякуємо, ${userName}е! До сплати ${payment} гривень`))
